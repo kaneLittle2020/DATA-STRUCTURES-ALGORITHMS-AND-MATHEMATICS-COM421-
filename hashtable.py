ced853d911f1af9a14e10580d6701e606351883b
kaneLittle2020 <5littk17@solent.ac.uk>
2020-10-28 14:41:53 +0000

    things

--- hashtable.py
+++ hashtable.py
@@ -1,12 +1,20 @@
 class Hashtable:
   def __init__(slef, capacity=127):
-    self.buckets = [None] * self.nbuckets
+    self.buckets = [None] *  self.nbuckets
 
   def put (self, key, value):
+    hash_code = self.getHashcode(key)
+
+
     pass
 
   def get (self, key):
     pass
+
+  def getHashcode (self, key):
+    for character in key:
+
+      pass
 
 table = Hashtable()
 table.put("cat", "A furry animal which goes meow")
